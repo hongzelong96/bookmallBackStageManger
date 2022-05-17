1b0141d92eb1a66d161e324237e7685824705912
Paul_hong <838647252@qq.com>
2022-05-17 22:04:33 +0800

    删除dist打包文件

--- dist/static/js/16.97cf4d336b6dd299aeda.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([16],{"0o3T":function(e,t){},"6R2X":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var i={render:function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("this is promotionList")])},staticRenderFns:[]};var r=n("VU/8")({},i,!1,function(e){n("0o3T")},null,null);t.default=r.exports}});
-//# sourceMappingURL=16.97cf4d336b6dd299aeda.js.map
--- dist/static/js/18.f1c5e329467cc694965f.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([18],{tvR6:function(n,c){}});
-//# sourceMappingURL=18.f1c5e329467cc694965f.js.map
